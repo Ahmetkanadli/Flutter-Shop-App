2211cee42ec15559efb48cc7cd3a0b721030f69a
Ahmetkanadli <94637438+Ahmetkanadli@users.noreply.github.com>
2024-03-06 16:44:38 +0300

    bottomNavigationBar oluşturuldu

--- flutter_shop_app/lib/pages/application/applicaiton_page.dart
+++ flutter_shop_app/lib/pages/application/applicaiton_page.dart
@@ -21,53 +21,78 @@ class _ApplicationPageState extends State<ApplicationPage> {
       child: SafeArea(
         child: Scaffold(
           body: buildPage(_index),
-          bottomNavigationBar: BottomNavigationBar(
-              currentIndex: _index,
-              onTap: (value){
-                setState(() {
-                  _index = value;
-                });
-                print(_index);
-              },
-
-              selectedItemColor: AppColors.primaryElement,
-              unselectedItemColor: AppColors.primaryFourthElementText,
-              elevation: 0,
-              items: [
-                BottomNavigationBarItem(
-                  label: "home",
-                    icon: SizedBox(
-                      width: 15.w,
-                      height: 15.w,
-                      child: Image.asset("assets/icons/home.png"),
-                    )
-                ),
-
-                BottomNavigationBarItem(
-                    label: "search",
-                    icon: SizedBox(
-                      width: 15.w,
-                      height: 15.w,
-                      child: Image.asset("assets/icons/search.png"),
-                    )
-                ),
-                BottomNavigationBarItem(
-                    label: "search",
-                    icon: SizedBox(
-                      width: 15.w,
-                      height: 15.w,
-                      child: Image.asset("assets/icons/chat.png"),
-                    )
-                ),
-                BottomNavigationBarItem(
-                    label: "search",
-                    icon: SizedBox(
-                      width: 15.w,
-                      height: 15.w,
-                      child: Image.asset("assets/icons/profile.png"),
-                    )
+          bottomNavigationBar: Container(
+            width: 375.w,
+            height: 58.h,
+            decoration: BoxDecoration(
+              color: Colors.grey.withOpacity(0.1),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.h),
+                topRight: Radius.circular(20.h)
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  spreadRadius: 1,
+                  blurRadius: 1,
                 )
               ]
+            ),
+            child: BottomNavigationBar(
+                currentIndex: _index,
+                onTap: (value){
+                  setState(() {
+                    _index = value;
+                  });
+                },
+
+                selectedItemColor: AppColors.primaryElement,
+                unselectedItemColor: AppColors.primaryFourthElementText,
+                elevation: 0,
+                items: [
+                  BottomNavigationBarItem(
+                    label: "home",
+                      icon: SizedBox(
+                        width: 15.w,
+                        height: 15.w,
+                        child: Image.asset("assets/icons/home.png"),
+                      )
+                  ),
+
+                  BottomNavigationBarItem(
+                      label: "search",
+                      icon: SizedBox(
+                        width: 15.w,
+                        height: 15.w,
+                        child: Image.asset("assets/icons/search.png"),
+                      )
+                  ),
+                  BottomNavigationBarItem(
+                      label: "search",
+                      icon: SizedBox(
+                        width: 15.w,
+                        height: 15.w,
+                        child: Image.asset("assets/icons/play-circle1.png"),
+                      )
+                  ),
+                  BottomNavigationBarItem(
+                      label: "search",
+                      icon: SizedBox(
+                        width: 15.w,
+                        height: 15.w,
+                        child: Image.asset("assets/icons/message-circle.png"),
+                      )
+                  ),
+                  BottomNavigationBarItem(
+                      label: "search",
+                      icon: SizedBox(
+                        width: 15.w,
+                        height: 15.w,
+                        child: Image.asset("assets/icons/person2.png"),
+                      )
+                  )
+                ]
+            ),
           ),
         ),
       ),
